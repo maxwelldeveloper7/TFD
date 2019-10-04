f1ad665d1a44e5eb65d8c2e3c8b4527be61fad62
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2019-10-04 11:27:22 -0300

    Início

---
+++ README.md
@@ -0,0 +1,21 @@
+# TFD - Tratamento Fora do Domicílio
+
+- Cadastro de Clientes
+- Agenda de Viagens com Carro
+- Liberação de Passagens
+
+
+    # Relatório de Viagens Realizadas
+    
+    - Quantidade de Pessoas
+    - Quantidade de Passagens
+    - Com Acompanhante
+    - Sem Acompanhante
+
+    # Relatório de Gastos
+
+    - Por Período
+
+    # Relatório de Gastos
+
+    - Por Período
---
+++ src/tfd/controle/Controle.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tfd.controle;
+
+import com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel;
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ *
+ * @author Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
+ */
+public class Controle {
+    
+    public static Controle instanceSingleton = null;
+    
+    private Controle(){
+    }
+    
+    public static Controle getInstance(){
+        if(instanceSingleton == null){
+            instanceSingleton = new Controle();
+        }
+        return instanceSingleton;
+    }
+    
+    public static void inicializar(){
+        WindowsLookAndFeel laf = new WindowsClassicLookAndFeel();
+        try {
+            UIManager.setLookAndFeel(laf);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Controle.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Falha ao setoar LookAndFell."+
+                    "/n"+ex.getMessage()+"/nA aplicação será encerrada.");
+            System.exit(0);
+        }
+        
+        JOptionPane.showMessageDialog(null, "Testando...");
+    }
+}
