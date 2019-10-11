a2fb613161bdace2d2d91e31dbf2bf6ddc023a73
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2019-10-11 16:41:49 -0300

    cadastro de motoristas

--- src/tfd/visao/FrmMotoristas.java
+++ src/tfd/visao/FrmMotoristas.java
@@ -1,6 +1,7 @@
 package tfd.visao;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Image;
@@ -34,8 +35,8 @@ public class FrmMotoristas extends JDialog{
     private JMenuBar menu;
     private JMenu mnuCadastro, mnuRelatorios, mnuSuporte;
     private JMenuItem mnuContato;
-    private TitledBorder bordaTitulo;
-    private JPanel pnAgenda, pnFundo, pnBotoes, pnDados, pnSetorStatus, pnCargoStatus, pnPerfilStatus;
+    private TitledBorder bordaTabela, bordaDados;
+    private JPanel pnTabela, pnFundo, pnBotoes, pnDados;
     private JButton btEditar, btAgendar, btVisualizar;
     private Icon icoAgendar, icoEditar, icoVisualizar, icoContato;
     private JLabel lbServidor, lbSetor, lbCargo, lbPerfil;
@@ -51,7 +52,7 @@ public class FrmMotoristas extends JDialog{
         Image iconeTitulo = Toolkit.getDefaultToolkit().getImage(url);//objeto imagem
         setIconImage(iconeTitulo);//define uma imagem para o icone
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();//pega a dimensão da tela
-        setBounds((screenSize.width - 800)/2, (screenSize.height - 600)/2, 800, 600);//define o tamanho da janela e posiciona ao centro
+        setBounds((screenSize.width - 800)/2, (screenSize.height - 400)/2, 800, 400);//define o tamanho da janela e posiciona ao centro
         setResizable(false);//impossibilita redimencionamento
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);//define ação ao fechar janela.
         construirComponentes();
@@ -59,7 +60,26 @@ public class FrmMotoristas extends JDialog{
 
     //Inicializando, definindo e posicionando componentes
     private void construirComponentes() {
+      pnTabela = new JPanel(null);
+      pnTabela.setBounds(4, 4, 786, 200);
+      bordaTabela = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED),"Motoristas");
+      pnTabela.setBorder(bordaTabela);
       
-        
+      pnDados = new JPanel(null);
+      pnDados.setBounds(4, 208, 786, 100);
+      bordaDados = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED),"Dados");
+      pnDados.setBorder(bordaDados);
+      
+      pnBotoes = new JPanel(null);
+      pnBotoes.setBounds(4, 308, 786, 50);
+      pnBotoes.setBorder(BorderFactory.createEtchedBorder());
+      
+      
+      pnFundo = new JPanel(null);
+      pnFundo.add(pnTabela);
+      pnFundo.add(pnDados);
+      pnFundo.add(pnBotoes);
+      pnFundo.setBackground(Color.gray);
+      getContentPane().add(pnFundo);
     }
 }
