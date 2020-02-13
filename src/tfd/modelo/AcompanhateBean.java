166eeb41cb688f67726ca18c562f36c5eba29ce1
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-02-13 19:01:14 -0300

    classe AcompanhanteBean.java

---
+++ src/tfd/modelo/AcompanhateBean.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tfd.modelo;
+
+
+public class AcompanhateBean {
+    private Integer id;
+    private String nome;
+    private String rg;
+    private String cpf;
+    private String endereco;
+
+    public AcompanhateBean() {
+    }
+
+    public AcompanhateBean(Integer id, String nome, String rg, String cpf, String endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.rg = rg;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public AcompanhateBean(String nome, String rg, String cpf, String endereco) {
+        this.nome = nome;
+        this.rg = rg;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    
+}
